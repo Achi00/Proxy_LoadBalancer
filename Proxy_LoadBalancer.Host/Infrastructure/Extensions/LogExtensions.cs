b25e201fa7c7efc43a2a336eb0735b0cb208a77d
Achi00 <achikoogorgadze@gmail.com>
2026-04-05 21:10:05 +0400

    update logging files

--- Proxy_LoadBalancer.Host/Infrastructure/Extensions/LogExtensions.cs
+++ Proxy_LoadBalancer.Host/Infrastructure/Extensions/LogExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Serilog;
+using Serilog.Events;
 
 namespace Proxy_LoadBalancer.Host.Infrastructure.Extensions
 {
@@ -16,7 +17,16 @@ namespace Proxy_LoadBalancer.Host.Infrastructure.Extensions
                         // new file each day
                         rollingInterval: RollingInterval.Day,
                         // keep last 7 days
-                        retainedFileCountLimit: 7);
+                        retainedFileCountLimit: 7)
+                    .WriteTo.Logger(lc => lc
+                    .Filter.ByIncludingOnly(e =>
+                        e.Properties.ContainsKey("SourceContext") &&
+                        e.Properties["SourceContext"].ToString().Contains("ActiveHealthCheckWorker"))
+                    .WriteTo.File("logs/health-.log", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 7))
+                    .WriteTo.Logger(lc => lc
+                    .Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Warning ||
+                                             e.Level == LogEventLevel.Error)
+                    .WriteTo.File("logs/errors-.log", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 7));
             });
 
             return builder;
