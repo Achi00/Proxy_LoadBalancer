c9279e9acead790914fe4c02aa334c2541e2e245
Achi00 <achikoogorgadze@gmail.com>
2026-04-05 13:06:36 +0400

    added active health check

---
+++ Proxy_LoadBalancer.Infrastructure/Health/ActiveHealthCheckWorker.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Hosting;
+using Proxy_LoadBalancer.Infrastructure.Options;
+
+namespace Proxy_LoadBalancer.Infrastructure.Health
+{
+    public class ActiveHealthCheckWorker : BackgroundService
+    {
+        private readonly IHttpClientFactory _factory;
+        private readonly PassiveHealthTracker _healthTracker;
+        private readonly ProxyOption _options;
+
+        public ActiveHealthCheckWorker(IHttpClientFactory factory, PassiveHealthTracker healthTracker, ProxyOption options)
+        {
+            _factory = factory;
+            _healthTracker = healthTracker;
+            _options = options;
+        }
+        protected override async Task ExecuteAsync(CancellationToken ct)
+        {
+            while (!ct.IsCancellationRequested)
+            {
+                foreach (var cluster in _options.Clusters.Values)
+                {
+                    foreach (var destination in cluster.Destinations)
+                    {
+                        foreach (var route in _options.Routes.Where(r => r.ClusterId == cluster.Id))
+                        {
+                            await ProbeAsync(destination, route, ct);
+                        }
+                    }
+                }
+                await Task.Delay(TimeSpan.FromSeconds(10), ct);
+            }
+        }
+
+        private async Task ProbeAsync(DestinationOption destination, RouteOption route, CancellationToken ct)
+        {
+            try
+            {
+                var client = _factory.CreateClient("proxy");
+                var response = await client.GetAsync(destination.Address + route.HealthCheck.Path, ct);
+                if (response.IsSuccessStatusCode)
+                {
+                    _healthTracker.RecordSuccess(destination.Address);
+                }
+                else
+                {
+                    _healthTracker.RecordFailure(destination.Address);
+                }
+            }
+            catch
+            {
+                _healthTracker.RecordFailure(destination.Address);
+            }
+        }
+    }
+}
