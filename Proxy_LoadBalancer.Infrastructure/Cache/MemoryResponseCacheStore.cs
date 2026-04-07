8a95accfd9128257dc1a48b6b2744467d66fcbd0
Achi00 <achikoogorgadze@gmail.com>
2026-04-07 23:07:13 +0400

    added response cache model

---
+++ Proxy_LoadBalancer.Infrastructure/Cache/MemoryResponseCacheStore.cs
@@ -0,0 +1,6 @@
+﻿namespace Proxy_LoadBalancer.Infrastructure.Cache
+{
+    public class MemoryResponseCacheStore : IMemoryCache
+    {
+    }
+}
---
+++ Proxy_LoadBalancer.Infrastructure/Models/CachedResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Proxy_LoadBalancer.Infrastructure.Models
+{
+    public sealed class CachedResponse
+    {
+        public int StatusCode { get; init; }
+
+        // includes selective headers only
+        public Dictionary<string, string[]> Headers { get; init; } = new();
+
+        public byte[] Body { get; init; } = [];
+
+        public DateTimeOffset CachedAt { get; init; }
+
+        public string[] VaryHeaderNames { get; init; } = [];
+    }
+}
